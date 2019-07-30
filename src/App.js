33c2368896e01b37c163445192a590398b28b04a
Horacio Herrera <me@hherrerag.com>
2019-07-30 10:30:23 +0200

    change env variable

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>api key: {process.env.API_KEY}</p>
+        <p>api key: {process.env.REACT_APP_API_KEY}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
